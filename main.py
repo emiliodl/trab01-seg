893af97e07b53847e7227c67ef20e374cc67c3b8
milio <emiliods79@gmail.com>
2024-05-10 13:44:42 -0300

    metodo para exportar chaves

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
-import os
 
 def generate_keys():
-    # Gerar o par de chaves
+    """
+    Gera um par de chaves privada e pública.
+    """
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
         backend=default_backend()
     )
     public_key = private_key.public_key()
+    return private_key, public_key
 
-    # Salvar a chave privada
-    with open("private_key.pem", "wb") as f:
-        f.write(private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        ))
+def save_private_key(private_key, filename="private_key.pem", password=None):
+    """
+    Salva a chave privada em um arquivo, com a opção de adicionar uma senha para encriptação.
+    """
+    if password is not None:
+        encryption = serialization.BestAvailableEncryption(password.encode())
+    else:
+        encryption = serialization.NoEncryption()
+    
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=encryption
+    )
+    with open(filename, 'wb') as f:
+        f.write(pem)
 
-    # Salvar a chave pública
-    with open("public_key.pem", "wb") as f:
-        f.write(public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ))
+def save_public_key(public_key, filename="public_key.pem"):
+    """
+    Salva a chave pública em um arquivo.
+    """
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    with open(filename, 'wb') as f:
+        f.write(pem)
 
-def load_keys():
-    # Carregar a chave privada
-    with open("private_key.pem", "rb") as f:
+def load_private_key(filename="private_key.pem", password=None):
+    """
+    Carrega uma chave privada de um arquivo, com a opção de usar uma senha se a chave foi encriptada.
+    """
+    with open(filename, 'rb') as f:
         private_key = serialization.load_pem_private_key(
             f.read(),
-            password=None,
+            password=password.encode() if password else None,
             backend=default_backend()
         )
+    return private_key
 
-    # Carregar a chave pública
-    with open("public_key.pem", "rb") as f:
+def load_public_key(filename="public_key.pem"):
+    """
+    Carrega uma chave pública de um arquivo.
+    """
+    with open(filename, 'rb') as f:
         public_key = serialization.load_pem_public_key(
             f.read(),
             backend=default_backend()
         )
+    return public_key
 
-    return private_key, public_key
+# Exemplo de uso das funções
+private_key, public_key = generate_keys()
+save_private_key(private_key, password="senha_segura")  # Opção com senha
+save_public_key(public_key)
 
-generate_keys()
-private_key, public_key = load_keys()
+loaded_private_key = load_private_key(password="senha_segura")
+loaded_public_key = load_public_key()
